daa72426c55eb4b35b45772c0e0d4f284d6aeb66
DarkJutsu <ulloajose34@gmail.com>
2025-05-30 22:00:28 -0600

    feat(Automation/Module1/app/):Add styles in app_order_folder.py.

--- Automation/Module1/app/app_order_folder.py
+++ Automation/Module1/app/app_order_folder.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+import styles
 
 window=tk.Tk()
 window.geometry("500x700") # width x height
 window.resizable(False, False) # Prevents the window from being resized
+styles.main_styles(window) # Styles
 window.title("App Make Folders & Move Files")
+
+labelTitle=ttk.Label(window, text="App Make Folders & Move Files", style="title.Label")
+labelTitle.pack(pady=10)
 
 window.mainloop()
