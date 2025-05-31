4135915b8b9614723b10d4714472e3d7f6ad2773
DarkJutsu <ulloajose34@gmail.com>
2025-05-30 20:19:20 -0600

    feat(Automation/Module1):Make dirs & update route

--- Automation/Module1/order_files_folders.py
+++ Automation/Module1/order_files_folders.py
@@ -1,4 +1,12 @@
 import os
 import shutil
 
-route="C:/Users/samllo/Documents/code/Python/Automation/Module1/order_files_folders/"
+route="C:/Users/samllo/Documents/code/Python/Pruebas/Automation/Module1/files_order"
+
+folders=["Images", "PDF's", "Video", "Files Word", "Files TXT"]
+
+for folder in folders:
+    route_folder=os.path.join(route, folder)
+
+    if not os.path.exists(route_folder):
+        os.makedirs(route_folder)
