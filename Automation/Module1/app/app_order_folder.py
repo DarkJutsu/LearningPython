233ee1946933df0cdb7ed3bb03df69a8c535b15d
DarkJutsu <ulloajose34@gmail.com>
2025-05-30 21:47:35 -0600

    feat(Automation/Module1/app/):Add structure of window, size, title, unresizable.

---
+++ Automation/Module1/app/app_order_folder.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+window=tk.Tk()
+window.geometry("500x700") # width x height
+window.resizable(False, False) # Prevents the window from being resized
+window.title("App Make Folders & Move Files")
+
+window.mainloop()
