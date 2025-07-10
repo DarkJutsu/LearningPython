8f26c3af6f04bcc9d8f16b0f682813a5292e4371
DarkJutsu <ulloajose34@gmail.com>
2025-07-09 20:01:33 -0600

    feat(Automation/Module1/app/):Use function, class & styles.

--- Automation/Module1/app/app_order_folder.py
+++ Automation/Module1/app/app_order_folder.py
@@ -1,14 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
-import styles
+import styles, select_route
+from model.MyRoute import MyRoute
 
 window=tk.Tk()
 window.geometry("500x700") # width x height
 window.resizable(False, False) # Prevents the window from being resized
+window.configure(background="#1ABFED")
 styles.main_styles(window) # Styles
 window.title("App Make Folders & Move Files")
 
-labelTitle=ttk.Label(window, text="App Make Folders & Move Files", style="title.Label")
-labelTitle.pack(pady=10)
+def handle_route():
+    route=select_route.choose_route()
+    MyRoute().set_route(route)
+    label_select_route.config(text=route)
+
+label_title=ttk.Label(window, text="App Make Folders & Move Files", style="title.Label")
+label_title.pack(pady=10)
+
+btn_choose_route=ttk.Button(window, text="Choose Route", command=handle_route, style="choose_route.TButton")
+btn_choose_route.pack(pady=10)
+
+label_select_route = ttk.Label(window, text="...", style="choose_route.Label")
+label_select_route.pack(pady=10)
+
 
 window.mainloop()
