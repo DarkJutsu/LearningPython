d1198fb599d1f143a52df748e954d8e6b70f41d6
DarkJutsu <ulloajose34@gmail.com>
2025-07-09 19:57:31 -0600

    feat(Automation/Module1/app/styles):Add styles for label & button.

--- Automation/Module1/app/styles.py
+++ Automation/Module1/app/styles.py
@@ -2,5 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 
 def main_styles(window):
-    style = ttk.Style(window)
-    style.configure("title.Label", font=("Arial", 24))
+    style = ttk.Style(window) # Set the context of style in window
+
+    style.configure("title.Label", font=("Arial", 24), background="#1ABFED", foreground="#29200E") # Set styles for the title
+    style.configure("choose_route.Label", font=("Arial", 14), background="#1ABFED", foreground="#FFF")  # Set styles for the title
+    style.configure("choose_route.TButton", font=("Arial", 18), background="#2D2E4B", foreground="#2D2E4B")  # Set styles for the title
